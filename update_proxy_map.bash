ef6ac76480054dc0c52f141bd94566c92cf06fd8
Jesse Driskill <jesse.driskill@gmail.com>
2021-01-05 11:32:41 -0800

    ...

--- update_proxy_map.bash
+++ update_proxy_map.bash
@@ -5,14 +5,11 @@ repo='https://github.com/jessed/nginx_proxy_map.git'
 branch='main'
 
 cd $proxyDir
-if [[ ! -d .git ]]; then
-  git clone https://github.com/jessed/nginx_proxy_map.git .
-fi
 
-git checkout -b $branch
+git checkout
 git remote update
 
-UPSTREAM=${1:-'@{u}'}
+UPSTREAM=${1:-@{u}}
 LOCAL=$(git rev-parse @)
 REMOTE=$(git rev-parse "$UPSTREAM")
 BASE=$(git merge-base @ "$UPSTREAM")
@@ -20,8 +17,15 @@ BASE=$(git merge-base @ "$UPSTREAM")
 if [[ $LOCAL == $REMOTE ]]; then
   echo "Up-to-date"
 elif [[ $LOCAL == $BASE ]]; then
-  echo "Need to pull"
+  echo "Need to pull ($LOCAL -> $REMOTE)"
   echo git reset --hard origin/$branch
   echo git pull branch-name
 fi
 
+DATE=$(date +%s)
+sudo mv /etc/nginx/proxy_map.conf.* /etc/nginx/old
+sudo mv -f /etc/nginx/proxy_map.conf /etc/nginx/old/proxy_map.conf-${DATE}
+sudo cp proxy_map.conf /etc/nginx
+
+sudo nginx -t 2>/dev/null
+if [[ $? == 0 ]]; then sudo systemctl reload nginx; fi
