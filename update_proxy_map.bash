58b92a41331ca84236267d4350e5a74114267ee3
Jesse Driskill <jesse.driskill@gmail.com>
2021-01-05 11:58:44 -0800

    10

--- update_proxy_map.bash
+++ update_proxy_map.bash
@@ -6,7 +6,7 @@ branch='main'
 
 cd $proxyDir
 
-git checkout >/dev/null
+git checkout -q
 git remote update >/dev/null
 
 UPSTREAM=${1:-@{u}}
@@ -17,9 +17,9 @@ BASE=$(git merge-base @ "$UPSTREAM")
 if [[ $LOCAL == $REMOTE ]]; then
   echo "Up-to-date"
 elif [[ $LOCAL == $BASE ]]; then
-  echo "Need to pull ($LOCAL -> $REMOTE)"
-  git reset --hard origin/$branch
-  git pull
+  echo "Need to pull ($LOCAL -> $BASE)"
+  git reset -q --hard origin/$branch
+  git pull -q
 fi
 
 # See if the proxy_map.conf file has been updated
@@ -29,10 +29,14 @@ diff -q proxy_map.conf /etc/nginx/proxy_map.conf
 # and reload nginx
 if [[ $? != 0 ]]; then
   DATE=$(date +%s)
+  echo "Moving old backup to /etc/nginx/old"
   sudo mv /etc/nginx/proxy_map.conf-* /etc/nginx/old
+  echo "Backing up current proxy map (/etc/nginx/proxy_map.conf-${DATE})"
   sudo mv -f /etc/nginx/proxy_map.conf /etc/nginx/proxy_map.conf-${DATE}
+  echo "Moving new proxy may into place"
   sudo cp proxy_map.conf /etc/nginx
 
-  sudo nginx -t 2>/dev/null
+  echo "Reloading nginx"
+  sudo nginx -tq
   if [[ $? == 0 ]]; then sudo systemctl reload nginx; fi
 fi
