554cdf59ab12a22cb89757007df959357ef0ca3a
Jesse Driskill <jesse.driskill@gmail.com>
2021-01-05 11:04:17 -0800

    script refinement

--- update_proxy_map.bash
+++ update_proxy_map.bash
@@ -1,9 +1,15 @@
 #! /bin/bash
 
 proxyDir=/opt/nginx_proxy_map
+repo='https://github.com/jessed/nginx_proxy_map.git'
+branch='main'
 
 cd $proxyDir
-git checkout 
+if [[ ! -d .git ]]; then
+  git clone https://github.com/jessed/nginx_proxy_map.git .
+fi
+
+git checkout -b $branch
 git remote update
 
 UPSTREAM=${1:-'@{u}'}
@@ -11,10 +17,11 @@ LOCAL=$(git rev-parse @)
 REMOTE=$(git rev-parse "$UPSTREAM")
 BASE=$(git merge-base @ "$UPSTREAM")
 
-if [ $LOCAL = $REMOTE ]; then
-    echo "Up-to-date"
-elif [ $LOCAL = $BASE ]; then
-    echo "Need to pull"
-    sh pull_code.sh
+if [[ $LOCAL == $REMOTE ]]; then
+  echo "Up-to-date"
+elif [[ $LOCAL == $BASE ]]; then
+  echo "Need to pull"
+  echo git reset --hard origin/$branch
+  echo git pull branch-name
 fi
 
