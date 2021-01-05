06633ec638475f4af5e7e8715bc94298cbd43dce
Jesse Driskill <jesse.driskill@gmail.com>
2021-01-05 10:55:40 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# NGINX+ Forward Proxy Map File
+
+## Overview
+This repository contains the mapping of domains and the appropriate proxy
+server for each.
---
+++ update_proxy_map.bash
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+proxyDir=/opt/nginx_proxy_map
+
+cd $proxyDir
+git checkout 
+git remote update
+
+UPSTREAM=${1:-'@{u}'}
+LOCAL=$(git rev-parse @)
+REMOTE=$(git rev-parse "$UPSTREAM")
+BASE=$(git merge-base @ "$UPSTREAM")
+
+if [ $LOCAL = $REMOTE ]; then
+    echo "Up-to-date"
+elif [ $LOCAL = $BASE ]; then
+    echo "Need to pull"
+    sh pull_code.sh
+fi
+
